dd6ca6276bf2af7e0dd9bd40d85930557cbfd349
Raviteja <raviteja8831@gmail.com>
2025-10-02 20:55:34 +0530

    changes as per prev

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import LoginScreen from './screens/LoginScreen';
+
+export default function IndexScreen() {
+  return <LoginScreen />;
+}
--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Redirect to login screen as the initial route
-  return <Redirect href="/login" />;
-}
