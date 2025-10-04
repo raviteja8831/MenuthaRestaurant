da58c5c25dfa771f9c798159d7fbe0a581fd56c7
Raviteja <raviteja8831@gmail.com>
2025-10-04 14:35:42 +0530

    index reverted

--- app/index.js
+++ app/index.js
@@ -1,14 +1,9 @@
 // Temporary instrumentation: log app start to help debug splash hang
 console.log('APP INDEX: module loaded', new Date().toISOString());
 
-import { AppRegistry } from 'react-native';
 import LoginScreen from './../src/screens/LoginScreen';
-import { name as appName } from '../app.json';
 
-function IndexScreen() {
+export default function IndexScreen() {
   console.log('APP INDEX: rendering IndexScreen');
   return <LoginScreen />;
 }
-
-// Register the root component with AppRegistry
-AppRegistry.registerComponent(appName, () => IndexScreen);
