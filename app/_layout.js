91399474f703ee2b03466be0fe5c0d8a617db342
Raviteja <raviteja8831@gmail.com>
2025-10-11 18:31:32 +0530

    gesture changes

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,6 @@
 
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
